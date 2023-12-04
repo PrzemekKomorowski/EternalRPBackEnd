14301944f764edc3e142cbaaf1d270a73e5a3a52
PrzemekKomorowski <106514184+PrzemekKomorowski@users.noreply.github.com>
2023-12-04 12:39:36 +0100

    First Edits

---
+++ EternalRpBackEnd/src/main/java/com/example/eternalrpbackend/EternalRpBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.eternalrpbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EternalRpBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EternalRpBackEndApplication.class, args);
+	}
+
+}
---
+++ EternalRpBackEnd/src/test/java/com/example/eternalrpbackend/EternalRpBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eternalrpbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EternalRpBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
